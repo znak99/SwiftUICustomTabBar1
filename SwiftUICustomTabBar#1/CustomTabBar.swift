85fe6058aa3012ecc38d551ae78c1e0f14ba4c8a
znak99 <xhahekcldhkznak0113@gmail.com>
2022-12-23 21:30:05 +0900

    Create tab bar

--- SwiftUICustomTabBar#1/CustomTabBar.swift
+++ SwiftUICustomTabBar#1/CustomTabBar.swift
@@ -8,10 +8,42 @@
 import SwiftUI
 
 struct CustomTabBar: View {
+    
+    
     var body: some View {
-        ZStack {
-            // some view
-            
+        GeometryReader { geo in
+            ZStack {
+                Color.black.ignoresSafeArea()
+                // some view
+                VStack {
+                    Spacer()
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 24)
+                            .fill(.white)
+                            .frame(
+                                width: geo.size.width / 1.3,
+                                height: geo.size.height / 20,
+                                alignment: .center
+                            )
+                        HStack {
+                            Button(action: {}) {
+                                Image(systemName: "house.fill")
+                            }
+                            Spacer()
+                            Button(action: {}) {
+                                Image(systemName: "gear")
+                            }
+                            Spacer()
+                            Button(action: {}) {
+                                Image(systemName: "person.fill")
+                            }
+                        }
+                        .foregroundColor(.gray)
+                        .frame(width: geo.size.width / 1.5)
+                        .font(.title2)
+                    }
+                }
+            }
         }
     }
 }

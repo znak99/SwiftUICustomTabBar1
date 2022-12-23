00b23d529bacbf2ccaa38c4eb7dac39bc4425741
znak99 <xhahekcldhkznak0113@gmail.com>
2022-12-23 21:08:26 +0900

    Initial Commit

---
+++ SwiftUICustomTabBar#1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUICustomTabBar#1
+//
+//  Created by cmStudent on 2022/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUICustomTabBar#1/SwiftUICustomTabBar_1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICustomTabBar_1App.swift
+//  SwiftUICustomTabBar#1
+//
+//  Created by cmStudent on 2022/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICustomTabBar_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
